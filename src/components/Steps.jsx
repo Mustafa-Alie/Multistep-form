24fc0bce8f53037cfe4d51236f5502008547e730
Mustafa-Alie <128284781+Mustafa-Alie@users.noreply.github.com>
2024-11-01 20:59:00 +0200

    Update Steps.jsx useReducer

--- src/components/Steps.jsx
+++ src/components/Steps.jsx
@@ -1,10 +1,12 @@
-function Steps({ step }) {
+function Steps({ state }) {
   return (
     <ul className="steps-ul list-group text-nowrap position-absolute d-flex flex-row flex-lg-column align-items-start">
       <li className="list-group-item bg-transparent border-0 d-flex justify-content-center align-items-center gap-3 ">
         <div
           className={`${
-            step === 1 ? "bg-active" : "text-white border border-1 border-white"
+            state.step === 1
+              ? "bg-active"
+              : "text-white border border-1 border-white"
           } num-container rounded-circle d-flex justify-content-center align-items-center`}
         >
           1
@@ -19,7 +21,9 @@ function Steps({ step }) {
       <li className="list-group-item bg-transparent border-0 d-flex justify-content-center align-items-center gap-3 ">
         <div
           className={`${
-            step === 2 ? "bg-active" : "text-white border border-1 border-white"
+            state.step === 2
+              ? "bg-active"
+              : "text-white border border-1 border-white"
           } num-container rounded-circle d-flex justify-content-center align-items-center`}
         >
           2
@@ -34,7 +38,9 @@ function Steps({ step }) {
       <li className="list-group-item bg-transparent border-0 d-flex justify-content-center align-items-center gap-3 ">
         <div
           className={`${
-            step === 3 ? "bg-active" : "text-white border border-1 border-white"
+            state.step === 3
+              ? "bg-active"
+              : "text-white border border-1 border-white"
           } num-container rounded-circle d-flex justify-content-center align-items-center`}
         >
           3
@@ -49,7 +55,9 @@ function Steps({ step }) {
       <li className="list-group-item bg-transparent border-0 d-flex justify-content-center align-items-center gap-3 ">
         <div
           className={`${
-            step === 4 ? "bg-active" : "text-white border border-1 border-white"
+            state.step === 4
+              ? "bg-active"
+              : "text-white border border-1 border-white"
           } num-container rounded-circle d-flex justify-content-center align-items-center`}
         >
           4
