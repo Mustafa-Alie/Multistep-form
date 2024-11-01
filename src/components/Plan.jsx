00384219f2c7a574c3f47226db394ff4c79a064e
Mustafa-Alie <128284781+Mustafa-Alie@users.noreply.github.com>
2024-11-01 20:58:42 +0200

    Update Plan.jsx useReducer

--- src/components/Plan.jsx
+++ src/components/Plan.jsx
@@ -1,6 +1,6 @@
 import { Form, Button } from "react-bootstrap";
 
-function Plan({ setCycle, cycle, setStep, setPlan, plan }) {
+function Plan({ state, dispatch }) {
   return (
     <div className="form-container mx-auto col-lg-7 d-flex flex-column justify-content-around pe-lg-5 p-3 pt-4 mb-4 mb-lg-0">
       <div>
@@ -12,80 +12,84 @@ function Plan({ setCycle, cycle, setStep, setPlan, plan }) {
 
       <div className="container">
         <div className="row">
-          <div className={`${
-              cycle === "monthly" ? "gap-3" : "gap-1"
-            } d-flex flex-column flex-lg-row justify-content-between`}>
+          <div
+            className={`${
+              state.cycle === "monthly" ? "gap-3" : "gap-1"
+            } d-flex flex-column flex-lg-row justify-content-between`}
+          >
             <div
-              onClick={() => setPlan("arcade")}
+              onClick={() => dispatch({ type: "SET_PLAN", payload: "arcade" })}
               className={`${
-                plan === "arcade" ? "bg-info" : ""
+                state.plan === "arcade" ? "bg-info" : ""
               } hover-container p-3 flex-fill  rounded-4 border  border-2 border-light-subtle d-flex flex-lg-column gap-4`}
             >
               <img
                 className="img-fluid align-self-start"
-                src={`${import.meta.env.BASE_URL}images/icon-arcade.svg`}
+                src="/images/icon-arcade.svg"
               ></img>
 
               <div>
                 <h3 className="text-primary fs-6 m-0">Arcade</h3>
                 <p className="m-0 text-tertiary" style={{ fontSize: "0.9rem" }}>
-                  {cycle === "monthly" ? "$9/mo" : "$90/yr"}
+                  {state.cycle === "monthly" ? "$9/mo" : "$90/yr"}
                 </p>
                 <span
                   className="text-primary fw-normal text-nowrap"
                   style={{ fontSize: "0.8rem" }}
                 >
-                  {cycle === "yearly" && "2 months free"}
+                  {state.cycle === "yearly" && "2 months free"}
                 </span>
               </div>
             </div>
 
             <div
-              onClick={() => setPlan("advanced")}
+              onClick={() =>
+                dispatch({ type: "SET_PLAN", payload: "advanced" })
+              }
               className={`${
-                plan === "advanced" ? "bg-info" : ""
+                state.plan === "advanced" ? "bg-info" : ""
               } hover-container p-3 flex-fill  rounded-4 border  border-2 border-light-subtle d-flex flex-lg-column gap-4`}
             >
               <img
                 className="img-fluid align-self-start"
-                src={`${import.meta.env.BASE_URL}images/icon-advanced.svg`}
+                src="/images/icon-advanced.svg"
               ></img>
 
               <div>
                 <h3 className="text-primary fs-6 m-0">Advanced</h3>
                 <p className="m-0 text-tertiary" style={{ fontSize: "0.9rem" }}>
-                  {cycle === "monthly" ? "$12/mo" : "$120/yr"}
+                  {state.cycle === "monthly" ? "$12/mo" : "$120/yr"}
                 </p>
                 <span
                   className="text-primary fw-normal text-nowrap"
                   style={{ fontSize: "0.8rem" }}
                 >
-                  {cycle === "yearly" && "2 months free"}
+                  {state.cycle === "yearly" && "2 months free"}
                 </span>
               </div>
             </div>
 
             <div
-              onClick={() => setPlan("pro")}
+              onClick={() => dispatch({ type: "SET_PLAN", payload: "pro" })}
               className={`${
-                plan === "pro" ? "bg-info" : ""
+                state.plan === "pro" ? "bg-info" : ""
               } hover-container p-3 flex-fill  rounded-4 border  border-2 border-light-subtle d-flex flex-lg-column gap-4`}
             >
               <img
                 className="img-fluid align-self-start"
-                src={`${import.meta.env.BASE_URL}images/icon-pro.svg`}
+                src="/images/icon-pro.svg"
               ></img>
 
               <div>
                 <h3 className="text-primary fs-6 m-0">Pro</h3>
                 <p className="m-0 text-tertiary" style={{ fontSize: "0.9rem" }}>
-                  {cycle === "monthly" ? "$15/mo" : "$150/yr"}
+                  {state.cycle === "monthly" ? "$15/mo" : "$150/yr"}
                 </p>
                 <span
                   className="text-primary fw-normal text-nowrap"
                   style={{ fontSize: "0.8rem" }}
                 >
-                  {cycle === "yearly" && "2 months free"}
+                  {state.cycle === "yearly" && "2 months free"}
                 </span>
               </div>
             </div>
@@ -96,17 +100,11 @@ function Plan({ setCycle, cycle, setStep, setPlan, plan }) {
             <span className="text-tertiary fw-semibold">Monthly</span>
 
             <Form.Check
-              checked={cycle === "yearly"}
-              onChange={() =>
-                setCycle(cycle === "monthly" ? "yearly" : "monthly")
-              }
+              // checked={}
+              onChange={() => dispatch({ type: "SET_CYCLE" })}
               type="switch"
               id="mySwitch"
               className="custom-switch"
-              onClick={() => {
-                if (cycle === "monthly") setCycle("yearly");
-                else setCycle("monthly");
-              }}
             />
 
             <span className="text-primary fw-semibold">Yearly</span>
@@ -119,7 +117,7 @@ function Plan({ setCycle, cycle, setStep, setPlan, plan }) {
           variant="link"
           type="Button"
           className="back-link text-tertiary"
-          onClick={() => setStep((prevStep) => prevStep - 1)}
+          onClick={() => dispatch({ type: "PREVIOUS_STEP" })}
         >
           Go Back
         </Button>
@@ -129,9 +127,7 @@ function Plan({ setCycle, cycle, setStep, setPlan, plan }) {
           size="lg"
           type="Button"
           className="text-white "
-          onClick={() => {
-            setStep((prevStep) => prevStep + 1);
-          }}
+          onClick={() => dispatch({ type: "NEXT_STEP" })}
         >
           Next Step
         </Button>
